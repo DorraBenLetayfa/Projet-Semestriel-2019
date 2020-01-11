ba8a7924a963a1631ebc58b8c9ed1e6b8e808d69
GhofraneHraiech <hraiech.ghofrane@eniso.u-sousse.tn>
2020-01-11 21:55:05 +0100

    entreprise rectif

--- SemestrielProject2019/src/main/java/dao/documents/Entreprise.java
+++ SemestrielProject2019/src/main/java/dao/documents/Entreprise.java
@@ -1,7 +1,55 @@
 package dao.documents;
 
 import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.data.mongodb.core.mapping.Field;
+
+import javax.persistence.Id;
+import java.util.List;
+import java.util.Objects;
 
 @Document
 public class Entreprise {
+    @Id
+    @Field(value = "id")
+    private long id;
+    @Field(value = "EntrepriseName")
+    private String entrepriseName;
+
+    @Override
+    public String toString() {
+        return "Entreprise{" +
+                "id=" + id +
+                ", entrepriseName='" + entrepriseName + '\'' +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Entreprise)) return false;
+        Entreprise that = (Entreprise) o;
+        return getId() == that.getId() &&
+                Objects.equals(getEntrepriseName(), that.getEntrepriseName());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getId(), getEntrepriseName());
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getEntrepriseName() {
+        return entrepriseName;
+    }
+
+    public void setEntrepriseName(String entrepriseName) {
+        this.entrepriseName = entrepriseName;
+    }
 }
